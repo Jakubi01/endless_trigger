6c9158fcb9063ea7af3cca877f91c7a8f80b51fe
Jakubi01 <muleng141@gmail.com>
2026-05-19 18:48:14 +0900

    feat: cinemachine camera
    - dead zone setting
    
    - confiner implement

--- Assets/Scripts/Items/Projectile/ProjectilePoolManager.cs
+++ Assets/Scripts/Items/Projectile/ProjectilePoolManager.cs
@@ -30,7 +30,6 @@ namespace Items.Projectile
         private Projectile OnCreateProjectile()
         {
             Projectile instance = Instantiate(projectilePrefab, transform);
-            Debug.Log(instance);
             return instance;
         }
 
